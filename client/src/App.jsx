146bd06b08a0b0ac91ec708f5a75d919e930778c
Alejandro Molero <alemolero1993@gmail.com>
2025-11-12 20:38:22 +0100

    fix(client): App.jsx use normal quotes to fix build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
-﻿import React from \"react\";
-import { BrowserRouter as Router, Routes, Route, Navigate } from \"react-router-dom\";
-import Layout from \"./components/Layout\";
-import Home from \"./Home\";
-import LoginPage from \"./pages/Login\";
-import RegisterPage from \"./pages/Register\";
-import CollectionPage from \"./pages/Collection\";
+﻿import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./Home";
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
+import CollectionPage from "./pages/Collection";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route element={<Layout />}>
-          <Route path=\"/\" element={<Home />} />
-          <Route path=\"/login\" element={<LoginPage />} />
-          <Route path=\"/register\" element={<RegisterPage />} />
-          <Route path=\"/collection\" element={<CollectionPage />} />
-          <Route path=\"/coleccion\" element={<CollectionPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/collection" element={<CollectionPage />} />
+          <Route path="/coleccion" element={<CollectionPage />} />
         </Route>
-        <Route path=\"*\" element={<Navigate to=\"/\" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
